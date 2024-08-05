fad333af03b2c8e4a2f38858b39b43446b5ddbc0
NikitaCat <kit-2003@mail.ru>
2024-08-05 23:17:27 +0300

    eeee

---
+++ src/main/java/Car.java
@@ -0,0 +1,8 @@
+public class Car {
+    String carName;
+    int carSpeed;
+    Car(String carName, int carSpeed) {
+        this.carName = carName;
+        this.carSpeed = carSpeed;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,47 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scannerCar = new Scanner(System.in);
+
+        Car[] cars = new Car[3];
+        Race winner = new Race();
+        for (int i = 0 ; i < 3 ; i++){
+            System.out.println("Enter the car name№"+ (i+1) + ": ");
+            String carName = scannerCar.next();
+            int carSpeed;
+            while (true) {
+                System.out.println("Enter the car speed#" + (i+1) + ": ");
+
+                try {
+                    carSpeed = scannerCar.nextInt();
+                    if (carSpeed <= 250 && carSpeed >= 0) {
+                        break;
+                    } else {
+                        System.out.println("Speed is not correct, speed should be between (0 to 250): ");
+                    }
+                }catch (InputMismatchException e){
+                    scannerCar.nextLine();
+                    System.out.println("Invalid input. Please enter an integer value for speed.");
+
+                }
+
+
+            }
+            cars[i] = new Car(carName, carSpeed);
+            winner.GetWinner(cars[i].carName,cars[i].carSpeed);
+        }
+        System.out.println("The fastest car is: "+ winner.carName);
+
+// тест посмотреть
+ /*      for (Car car : cars) {
+
+            System.out.println(car.carName +" " + car.carSpeed);
+        }
+*/
+
     }
 }
